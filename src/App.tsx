9e8b25dae12e5589c0cfbc3b92851ab947b3cc95
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-13 21:30:48 -0300

    feat; add Home and React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { FunctionComponent } from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
 
-import Header from './components/Header';
-interface AppProps {
-  message: string
-}
 
-const App :FunctionComponent<AppProps> = ({message})=>{
+const App  = ()=>{
   return (
-    <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
