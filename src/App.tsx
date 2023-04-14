ce21111e87e82fbf888d7461095d87841d5405a4
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-13 21:20:28 -0300

    feat: Header Components, style header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from 'react';
+
+import Header from './components/Header';
+interface AppProps {
+  message: string
+}
+
+const App :FunctionComponent<AppProps> = ({message})=>{
+  return (
+    <Header/>
+  )
+}
+
+export default App
