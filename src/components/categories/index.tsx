bb4aba301f01fa531d906665503bc6759beb8579
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-13 21:43:31 -0300

    feat: add env config file

---
+++ src/components/categories/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Categories.css'
+import Category from '../../types/category.types'
+
+const Categories = ()=>{
+  const [categories,setCategories] = useState<Category[]>([])
+  
+  return(
+    <div className="categories-container">
+      <div className="categories-content">
+
+      </div>
+    </div>
+  )
+}
+
+export default Categories
