3189893d3439a9f9e4302da0aa5ca743fc20602f
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-13 22:05:38 -0300

    feat: add Component Category and config vite.cfg

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
 import './Categories.css'
 import Category from '../../types/category.types'
+import env from '../../config/env.config'
 
 const Categories = ()=>{
   const [categories,setCategories] = useState<Category[]>([])
-  
+
+  const fetchCategories = async () => {
+    try {
+      const {data} = await axios.get(`${env.apiUrl}/api/category`)
+      console.log(data)
+      setCategories(data) 
+      
+    } catch (error) {
+      console.log({error})
+    }
+  }
+
+  console.log(categories)
+
+  useEffect(()=>{
+    fetchCategories()
+  },[])
+
   return(
     <div className="categories-container">
       <div className="categories-content">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import Header from '../../components/Header'
+import Categories from '../../components/categories'
 
 const Home = ()=>{
   return (
     <div>
       <Header/>
+      
+      <Categories/>
     </div>
   )
 }
