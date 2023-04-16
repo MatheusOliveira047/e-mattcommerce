3683b75f199f1936a4cae07e69e6e3e7caa482be
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-16 20:31:31 -0300

    feat: add component Button

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,8 @@
+import {BsGoogle} from 'react-icons/bs'
+import CustomButton from '../../components/Custom-Button'
 import { LoginContainer,LoginContent,LoginHeadline,LoginInputContainer,LoginSubtitle} from './login.styled'
+
+
 
 const LoginPage = ()=>{
   return(
@@ -6,7 +10,7 @@ const LoginPage = ()=>{
     <LoginContainer>
       <LoginContent>
         <LoginHeadline> Entre com a sua conta </LoginHeadline>
-        {/**Button */}
+        <CustomButton startIcon={<BsGoogle size={18}/>}> Entrar com o Google </CustomButton>
         <LoginSubtitle> ou entre com o seu e-mail</LoginSubtitle>
         <LoginInputContainer>{/** email input*/}</LoginInputContainer>
         <LoginInputContainer>{/** email input*/}</LoginInputContainer>
