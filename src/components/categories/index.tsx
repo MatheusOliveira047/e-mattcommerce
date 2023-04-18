1550a23146c77fbf9f8fdbde0967caeee9a7b1c5
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-17 21:58:55 -0300

    feat: add firestore get from firestore db

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,19 +1,25 @@
 import { useState,useEffect } from 'react'
-import axios from 'axios'
+import {getDocs,collection} from 'firebase/firestore'
 
 import {CategoriesContainer,CategoriesContent} from './categories.styled'
 import Category from '../../types/category.types'
 import CategoryItem from '../category-item'
+import { db } from '../../config/firebase.config'
+import { categoryConverter } from '../../converters/firestore.converters'
 
 const Categories = ()=>{
   const [categories,setCategories] = useState<Category[]>([])
 
   const fetchCategories = async () => {
     try {
-      const {data} = await axios.get(`https://club-ecommerce-api.up.railway.app/api/category`)
-      console.log(data)
-      setCategories(data) 
+      const categoriesFromFirestore: Category[] = []
+
+     const querySnapShot =  await getDocs(collection(db,'categories').withConverter(categoryConverter))
       
+      querySnapShot.forEach((doc)=>{
+        categoriesFromFirestore.push(doc.data())
+      })
+      setCategories(categoriesFromFirestore)
     } catch (error) {
       console.log({error})
     }
