55b7892279673616373353a9b543e5f6f60b5a79
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-19 17:15:56 -0300

    feat: add context

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,30 @@ import Header from './components/Header';
 import './App.css'
 import SingUpPage from './pages/Sing-up';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './config/firebase.config';
+import { auth, db } from './config/firebase.config';
+import { FunctionComponent, useContext } from 'react';
+import { UserContext } from './contexts/user.context';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 
-const App  = ()=>{
-  onAuthStateChanged(auth,(user)=>{
-    console.log(user)
+const App: FunctionComponent  = ()=>{
+  const {currentUser,loginUser,isAuthenticated,logoutUser} = useContext(UserContext)
+
+
+  onAuthStateChanged(auth, async (user)=>{
+    const isSigningOut = isAuthenticated && !user
+
+    if(isSigningOut){
+      return logoutUser()
+    }
+
+    const isSigningIn = !isAuthenticated && user
+    if(isSigningIn){
+      const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+
+      return loginUser(userFromFirestore as any)
+    }
   })
   return (
     <BrowserRouter>
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import React from 'react'
+import UserContextProvider from './contexts/user.context'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>,
 )
