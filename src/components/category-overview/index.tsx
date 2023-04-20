0486e91d16351074a779b36cfb285d49a14aba3e
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-20 20:49:04 -0300

    feat: add loading

--- src/components/category-overview/index.tsx
+++ src/components/category-overview/index.tsx
@@ -1,22 +1,30 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import {CategoryContainer,CategoryTitle,ProductsContainer} from './category-overview.styled'
 import Category from '../../types/category.types'
 import ProductItem from '../product-item'
+import { UserContext } from '../../contexts/user.context'
+import { CategoryContext } from '../../contexts/category.context'
+import Loading from '../Loading'
 
 interface CategoryOverviewProps {
   category: Category
 }
 
 const CategoryOverview: FunctionComponent<CategoryOverviewProps> = ({category})=>{
+ const {isLoading} = useContext(CategoryContext)
+
   return (
-    <CategoryContainer>
-      <CategoryTitle>
-        {category.displayName}
-      </CategoryTitle>
-      <ProductsContainer>
-        {category.products.slice(0,4).map(product => <ProductItem product={product} key={product.id}/>)}
-      </ProductsContainer>
-    </CategoryContainer>
+    <>
+      {isLoading && <Loading/>}
+      <CategoryContainer>
+        <CategoryTitle>
+          {category.displayName}
+        </CategoryTitle>
+        <ProductsContainer>
+          {category.products.slice(0,4).map(product => <ProductItem product=      {product} key={product.id}/>)}
+        </ProductsContainer>
+      </CategoryContainer>
+    </>
   )
 }
 
