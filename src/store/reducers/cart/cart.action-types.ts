10628b262e6ebc437889dcb4a92949e5b54c56f1
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-04-30 18:54:10 -0300

    feat: add ts to Actions and Types

--- src/store/reducers/cart/cart.action-types.ts
+++ src/store/reducers/cart/cart.action-types.ts
@@ -3,10 +3,10 @@
 const CartActionsTypes = {
   TOGGLE_CART: 'cart/toggleCart' as const,
   ADD_PRODUCT_TO_CART: 'cart/addProductToCart' as const,
-  REMOVE_PRODUCT_FROM_CART: 'cart/removeProductFromCart',
-  INCREASE_PRODUCT_QUANTITY: 'cart/incleaseProductQuantity',
-  SUBTRACT_PRODUCT_QUANTITY: 'cart/subtractProductQuantity',
-  CLEAR_PRODUCTS: 'cart/clearProducts'
+  REMOVE_PRODUCT_FROM_CART: 'cart/removeProductFromCart' as const,
+  INCREASE_PRODUCT_QUANTITY: 'cart/incleaseProductQuantity' as const,
+  SUBTRACT_PRODUCT_QUANTITY: 'cart/subtractProductQuantity' as const,
+  CLEAR_PRODUCTS: 'cart/clearProducts' as const
 }
 
 export default CartActionsTypes
--- src/store/reducers/cart/cart.actions.ts
+++ src/store/reducers/cart/cart.actions.ts
@@ -9,36 +9,52 @@ interface ToggleCartAction {
 
 interface AddProductToCartAction {
     type: typeof CartActionsTypes.ADD_PRODUCT_TO_CART,
-    payload: CartProduct[]
+    payload: Product
+}
+
+interface RemoveProductFromCartAction {
+    type: typeof CartActionsTypes.REMOVE_PRODUCT_FROM_CART,
+    payload: string
+}
+interface IncreaseProductQuantityAction {
+    type: typeof CartActionsTypes.INCREASE_PRODUCT_QUANTITY,
+    payload: string
+}
+interface SubtractProductQuantityAction {
+    type: typeof CartActionsTypes.SUBTRACT_PRODUCT_QUANTITY,
+    payload: string
+}
+interface ClearProductsAction {
+    type: typeof CartActionsTypes.CLEAR_PRODUCTS
 }
 
 export const cartToggle = ():ToggleCartAction => ({
     type: CartActionsTypes.TOGGLE_CART
 })
 
-export const addProductToCart = (payload: Product)=>({
+export const addProductToCart = (payload: Product):AddProductToCartAction=>({
     type: CartActionsTypes.ADD_PRODUCT_TO_CART,
     payload
 })
 
-export const removeProductFromCart = (payload: string)=>({
+export const removeProductFromCart = (payload: string):RemoveProductFromCartAction=>({
     type:CartActionsTypes.REMOVE_PRODUCT_FROM_CART,
     payload
 })
 
-export const increaseProductQuantity = (payload:string)=>({
+export const increaseProductQuantity = (payload:string):IncreaseProductQuantityAction=>({
     type:CartActionsTypes.INCREASE_PRODUCT_QUANTITY,
     payload
 })
 
-export const subtractProductQuantity = (payload:string)=>({
+export const subtractProductQuantity = (payload:string):SubtractProductQuantityAction=>({
     type:CartActionsTypes.SUBTRACT_PRODUCT_QUANTITY,
     payload
 })
 
-export const clearProducts = ()=>({
+export const clearProducts = ():ClearProductsAction=>({
     type: CartActionsTypes.CLEAR_PRODUCTS
 })
 
 
-export type UserActions = ToggleCartAction
+export type CartActions = ToggleCartAction | AddProductToCartAction | RemoveProductFromCartAction | IncreaseProductQuantityAction | SubtractProductQuantityAction | ClearProductsAction
